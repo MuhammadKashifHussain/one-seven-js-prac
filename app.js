ed7cba3bc6e8e041421728432425e47de12b0a2e
ahmedraza <ahmedrazauc11@gmail.com>
2017-03-12 13:05:43 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,54 @@
+//alert("kashif");
+//console.log("Hussain");
+//console.log('my name is "kashif"');
+//var userName= 'mkh'
+//console.log(userName);
+
+//var num= 0;
+//console.log(num++ + ++num + num + --num);
+//console.log(num);
+
+
+//var num=13;
+//console.log(num++ + ++num - --num);
+//console.log(num);
+//console.log('kashif');
+//console.error('kashif');
+//console.warn('kashif');
+//console.log(13  % 7);
+//console.log (10 % 2);
+//console.log(10 % 3);
+//console.log(10 % 4);
+//var userName = prompt('what is your name?' ,'kashif')
+//console.log(userName)
+//var userName = confirm('Are you ready')
+ //console.log(userName)
+//var A=prompt ('what is your num?');
+//var B=prompt ('what is your num?');
+//console.log(A/B);
+//var marks=prompt('what is marks?');
+
+var A=+prompt('what is your num?');
+var B=prompt('what do you want?');
+var C=+prompt('what si your num?');
+if (B=="+"){
+    console.log('A+C');
+}
+else if (B=="-"){
+    console.log(A-C);
+}
+else if(B=="*"){
+    console.log(A*C);
+}
+
+else if (B=="/"){
+    console.log(A/C);
+}
+else if (B=="%"){
+    console.log(A%C);
+}
+
+
+
+
+
